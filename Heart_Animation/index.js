0edaed5e1ce8738b749f34d781f5ee2ed55b87c9
Kishor S <kishorsbca@gmail.com>
2023-06-21 22:22:26 +0530

    adding name

--- Heart_Animation/index.js
+++ Heart_Animation/index.js
@@ -7,10 +7,20 @@ bodyEl.addEventListener("mousemove",(e)=>{
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
     const size = Math.random()*100;
-    spanEl.style.width = size+"px"
-    spanEl.style.height = size+"px"
+    spanEl.style.width = size+20+"px"
+    spanEl.style.height = size+20+"px"
     setTimeout(() => {
         spanEl.remove();
     }, 3000);
+    const Span = document.createElement("h3");
+    Span.style.left = xPos+10 + "px";
+    Span.style.top = yPos+10 + "px";
+    const Size1 = Math.random()*100;
+    Span.style.width = Size1+20+"px"
+    Span.style.height = Size1+20+"px"
+    setTimeout(() => {
+        Span.remove();
+    }, 3000);
     bodyEl.appendChild(spanEl);
+    bodyEl.appendChild(Span);
 })
